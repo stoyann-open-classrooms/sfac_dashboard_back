a48d2f38e57bbb4da833706f6972775adcc68fcf
Stoyann <stoyann.openclassrooms@gmail.com>
2022-06-03 11:46:09 +1100

    fermeture des routes _ bearer token

--- server.js
+++ server.js
@@ -15,32 +15,40 @@ app.use("/Images", express.static("./Images"));
 
 // fournisseur
 const fournisseurRouter = require("./routes/fournisseurRouter");
-app.use(`${process.env.BASE_URL}/fournisseur`, fournisseurRouter);
+app.use(
+  `${process.env.BASE_URL}/fournisseur`,
+  checkTokenMiddleware,
+  fournisseurRouter
+);
 // demande
 const demandeRouter = require("./routes/demandeRouter");
-app.use(`${process.env.BASE_URL}/demande`, demandeRouter);
+app.use(`${process.env.BASE_URL}/demande`, checkTokenMiddleware, demandeRouter);
 // kanban
 const kanbanRouter = require("./routes/kanbanRouter");
 app.use(`${process.env.BASE_URL}/kanban`, checkTokenMiddleware, kanbanRouter);
 // FRSPDT
 const frspdtRouter = require("./routes/frspdtRouter");
-app.use(`${process.env.BASE_URL}/frspdt`, frspdtRouter);
+app.use(`${process.env.BASE_URL}/frspdt`, checkTokenMiddleware, frspdtRouter);
 // produit
 const produitRouter = require("./routes/produitRouter");
-app.use(`${process.env.BASE_URL}/produit`, produitRouter);
+app.use(`${process.env.BASE_URL}/produit`, checkTokenMiddleware, produitRouter);
 // unite
 const uniteRouter = require("./routes/uniteRouter");
-app.use(`${process.env.BASE_URL}/unite`, uniteRouter);
+app.use(`${process.env.BASE_URL}/unite`, checkTokenMiddleware, uniteRouter);
 // user
 // categorie
 const categorieRouter = require("./routes/categorieRouteur");
-app.use(`${process.env.BASE_URL}/categorie`, categorieRouter);
+app.use(
+  `${process.env.BASE_URL}/categorie`,
+  checkTokenMiddleware,
+  categorieRouter
+);
 // user
 const userRouter = require("./routes/userRouter");
-app.use(`${process.env.BASE_URL}/users`, userRouter);
+app.use(`${process.env.BASE_URL}/users`, checkTokenMiddleware, userRouter);
 // auth
 const authRouter = require("./routes/authRouter.js");
-const { attachment } = require("express/lib/response");
+// const { attachment } = require("express/lib/response");
 app.use(`${process.env.BASE_URL}/auth`, authRouter);
 
 // ++++++++++++++++++++++++++++ demmarage du serveur  ++++++++++++++++++++++++++++
